8c731a704a06813c1465d77203b3eccc116e5784
Lodewijk Schutte <lodewijk@gmail.com>
2011-01-12 15:55:51 +0100

    Added EE2 version

---
+++ ee2/dropdate/language/english/dropdate_lang.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Fieldtype enabling users to select a date using 3 drop-downs (day, month, year).
+ *
+ * @package   	DropDate
+ * @author    	Stephen Lewis <addons@experienceinternet.co.uk>
+ * @copyright 	Copyright (c) 2010, Stephen Lewis
+ * @link      	http://experienceinternet.co.uk/dropdate/
+ */
+
+$lang = array(
+	
+// General.
+'save_format_label'	=> 'Save Date As:',
+'unix_format_label'	=> 'UNIX timestamp',
+'ymd_format_label'	=> 'YYYYMMDD',
+'year_range_label'	=> 'Year range:',
+
+'year'	=> 'Year',
+'month'	=> 'Month',
+'day'	=> 'Day',
+	
+// Months.
+'jan'	=> 'January',
+'feb'	=> 'February',
+'mar'	=> 'March',
+'apr'	=> 'April',
+'may'	=> 'May',
+'jun'	=> 'June',
+'jul'	=> 'July',
+'aug'	=> 'August',
+'sep'	=> 'September',
+'oct'	=> 'October',
+'nov'	=> 'November',
+'dec'	=> 'December',
+
+// All done.
+'' => ''
+	
+);
+
+/* End of file		: dropdate_lang.php */
+/* File location	: /system/expressionengine/third_party/dropdate/language/english/dropdate_lang.php */
