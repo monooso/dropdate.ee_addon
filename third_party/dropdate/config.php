47d9f58959ed535e1828e27409fdce347406258e
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-10-25 17:10:06 +0100

    Update version number.

--- third_party/dropdate/config.php
+++ third_party/dropdate/config.php
@@ -6,14 +6,14 @@
  * @author          Stephen Lewis (http://github.com/experience/)
  * @copyright       Experience Internet
  * @package         Dropdate
- * @version         2.3.0a1
+ * @version         3.0.0a1
  */
 
 if ( ! defined('DROPDATE_NAME'))
 {
   define('DROPDATE_NAME', 'Dropdate');
   define('DROPDATE_TITLE', 'DropDate');
-  define('DROPDATE_VERSION', '2.3.0a1');
+  define('DROPDATE_VERSION', '3.0.0a1');
 }
 
 $config['name']    = DROPDATE_NAME;
