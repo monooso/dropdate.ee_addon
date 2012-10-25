9700e231df2cec5b32a16ea58b4c1512b8a429c8
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-10-25 18:18:04 +0100

    Smart quotes.

--- third_party/dropdate/language/english/dropdate_ft_lang.php
+++ third_party/dropdate/language/english/dropdate_ft_lang.php
@@ -25,8 +25,8 @@ $lang = array(
   'label__time_15'     => 'Yes, in 15 minute intervals',
 
   'message__no_global_settings' => "<p>We lured you here under false pretences.
-    <p>There are no global settings, but ExpressionEngine won't update a fieldtype unless you visit its global settings page. Obviously.</p>
-    <p>Just click the 'Submit' button, and get back to your nice life where you don't have to deal with this nonsense for a living.</p>",
+    <p>There are no global settings, but ExpressionEngine won&rsquo;t update a fieldtype unless you visit its global settings page. Obviously.</p>
+    <p>Just click the &ldquo;Submit&rdquo; button, and get back to your nice life where you don&rsquo;t have to deal with this nonsense for a living.</p>",
 
   // Field.
   'label__day'      => 'Day',
